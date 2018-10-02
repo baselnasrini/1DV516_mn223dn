05cceeac5f611cec4265f0df3c8e0595b0431902
Mohammed Basel Nasrini <beso_o@live.com>
2018-10-02 18:56:04 +0200

    First commit

---
+++ src/assign1/A1SequenceWithMinimum.java
@@ -0,0 +1,9 @@
+package assign1;
+
+public interface A1SequenceWithMinimum {
+	public void insertRight(int value); 
+	public int removeRight();
+	public void insertLeft(int value);
+	public int removeLeft();
+	public int findMinimum();
+}
---
+++ src/assign1/A1SequenceWithMinimumMain.java
@@ -0,0 +1,44 @@
+package assign1;
+
+public class A1SequenceWithMinimumMain implements A1SequenceWithMinimum {
+
+	public A1SequenceWithMinimumMain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void insertRight(int value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int removeRight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void insertLeft(int value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int removeLeft() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int findMinimum() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
