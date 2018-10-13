5ef04ad8ab7b1ddced626a4e5d3748d9c7768950
Mohammed Basel Nasrini <beso_o@live.com>
2018-10-13 10:54:32 +0200

    Add classes

---
+++ src/assignment2/hashTable/MyHashTable.java
@@ -0,0 +1,27 @@
+package assignment2.hashTable;
+
+public class MyHashTable<T> implements A2HashTable<T> {
+
+	public MyHashTable() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void insert(T element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(T element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean contains(T element) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/assignment2/measure/MyMeasure.java
@@ -0,0 +1,27 @@
+package assignment2.measure;
+
+public class MyMeasure implements A2Measure {
+
+	public MyMeasure() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean isSameCollection(int[] array1, int[] array2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int minDifferences(int[] array1, int[] array2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int[] getPercentileRange(int[] arr, int lower, int upper) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
